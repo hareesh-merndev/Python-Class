81f25a66f65d3b089dcc7c505e5a4767d8e96b25
Hareesh Ragavan <hareeshragavan404@gmail.com>
2026-02-18 10:13:14 +0530

    Add files via upload

---
+++ Day3.py
@@ -0,0 +1,20 @@
+# fibonacci series
+a, b = 0, 1
+while a < 10:
+    print(a, end=' ')
+    a, b = b, a + b
+# # for leet
+# class Solution:
+#     def fib(self, n: int) -> int:
+#         if n == 0:
+#             return 0
+#         if n == 1:
+#             return 1
+        
+#         a, b = 0, 1
+#         for _ in range(2, n + 1):
+#             a, b = b, a + b
+        
+#         return b
+
+        
